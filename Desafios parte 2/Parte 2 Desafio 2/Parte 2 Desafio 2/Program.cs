4e0d2d7b49e9f3c13de1135c0ac3b65474004830
Diego MENDEZ Choj <106936869+DMendez455@users.noreply.github.com>
2025-02-28 23:27:55 -0600

    Add files via upload

---
+++ Desafios parte 2/Parte 2 Desafio 2/Parte 2 Desafio 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Juego
+{
+    private int puntajeRecord = 0;
+    private string jugadorRecord = "";
+
+    public void ActualizarPuntaje(int puntaje, string nombreJugador)
+    {
+        if (puntaje > puntajeRecord)
+        {
+            puntajeRecord = puntaje;
+            jugadorRecord = nombreJugador;
+            Console.WriteLine("La nueva puntuación más alta es " + puntajeRecord);
+            Console.WriteLine("La puntuación más alta fue lograda por " + jugadorRecord);
+        }
+        else
+        {
+            Console.WriteLine("La puntuación más alta de " + puntajeRecord + " no se ha podido superar, y aún está en manos de " + jugadorRecord);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Juego juego = new Juego();
+
+        juego.ActualizarPuntaje(150, "Pepito");  
+        juego.ActualizarPuntaje(100, "Duki"); 
+        juego.ActualizarPuntaje(200, "Khea");   
+    }
+}
---
+++ Desafios parte 2/Parte 2. Desafio 1/Parte 2. Desafio 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static string registeredUsername = "";
+    static string registeredPassword = "";
+
+    static void Main()
+    {
+        RegisterUser();
+        LoginUser();
+    }
+
+    static void RegisterUser()
+    {
+        Console.WriteLine("Registro de usuario");
+        Console.Write("Ingrese un nombre de usuario: ");
+        registeredUsername = Console.ReadLine();
+
+        Console.Write("Ingrese una contraseña: ");
+        registeredPassword = Console.ReadLine();
+
+        Console.WriteLine("Registro exitoso!\n");
+    }
+
+    static void LoginUser()
+    {
+        Console.WriteLine("Inicio de sesión");
+        Console.Write("Ingrese su nombre de usuario: ");
+        string username = Console.ReadLine();
+
+        Console.Write("Ingrese su contraseña: ");
+        string password = Console.ReadLine();
+
+        if (username == registeredUsername && password == registeredPassword)
+        {
+            Console.WriteLine("Inicio de sesión exitoso! Bienvenido, " + username + "\n");
+        }
+        else
+        {
+            Console.WriteLine("Error: Nombre de usuario o contraseña incorrectos.\n");
+        }
+    }
+}
+;
